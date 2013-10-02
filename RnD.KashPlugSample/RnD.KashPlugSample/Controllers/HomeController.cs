699554122cd7c14b20634d0fb109adc48f6ab616
Rasel Ahmmed Bappi <rasel.bappi@hotmail.com>
2013-10-02 16:23:59 +0600

    2013-10-02

--- RnD.KashPlugSample/RnD.KashPlugSample/Controllers/HomeController.cs
+++ RnD.KashPlugSample/RnD.KashPlugSample/Controllers/HomeController.cs
@@ -10,12 +10,11 @@ namespace RnD.KashPlugSample.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Welcome to ASP.NET MVC!";
-
             return View();
         }
 
-        public ActionResult About()
+        //LogOut
+        public ActionResult LogOut()
         {
             return View();
         }
