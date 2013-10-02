292c931f9430587544195fd85308f795b828dcca
raselahmmed <raselahmmed@hotmail.com>
2013-10-02 01:59:47 -0700

    2013-10-02

--- RnD.KashPlugSample/RnD.KashPlugSample/Models/CostsOrExpense.cs
+++ RnD.KashPlugSample/RnD.KashPlugSample/Models/CostsOrExpense.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace RnD.KashPlugSample.Models
 {
